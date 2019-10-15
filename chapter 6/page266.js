e3856783b14cb83cfbaf31aed8ca3bf0ec98fb03
«233blr» <233blr@gmail.com>
2019-10-15 20:43:27 +0300

    add new pages

--- chapter 6/page266.js
+++ chapter 6/page266.js
@@ -1,2 +1,9 @@
-var planet = document.getElementById("greenplanet");
-planet.innerHTML = "Red Alert: hit by phaser fire!";
+function chengeValue() {
+  var planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Red Alert: hit by phaser fire!";
+}
+
+function chengeValueTwo() {
+  var planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Всё вернули";
+}
