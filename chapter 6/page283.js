c63899fd068ca5add1750389420300f121c062cd
«233blr» <233blr@gmail.com>
2019-10-15 21:09:49 +0300

    add setAttribute

---
+++ chapter 6/page283.js
@@ -0,0 +1,10 @@
+function chengeValue() {
+  var planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Red Alert: hit by phaser fire!";
+  planet.setAttribute("class", "redtext");
+}
+
+function chengeValueTwo() {
+  var planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Всё вернули";
+}
